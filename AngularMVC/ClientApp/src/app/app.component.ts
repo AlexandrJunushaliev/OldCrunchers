52b697e33a74cdde8e587460861b528d231416d6
Hikirangi <dzhunall@mail.ru>
2019-10-29 00:12:49 +0300

    [ADD] Example of categories and improved look

--- AngularMVC/ClientApp/src/app/app.component.ts
+++ AngularMVC/ClientApp/src/app/app.component.ts
@@ -4,6 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent {
+  static Category = class {
+  public name:string;
+  public route:string;
+  constructor(name:string,route:string)
+  {
+    this.name=name;
+    this.route=route;
+  }
+};
+
   title = 'app';
+  public items = [new AppComponent.Category("mobile","/counter"),new AppComponent.Category("computers","/counter")];
+  public showCat()
+  {
+
+  }
 }
