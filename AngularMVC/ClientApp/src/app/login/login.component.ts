e547e53a859e873ad9b5f5b696ec0f1a270363a6
Hikirangi <dzhunall@mail.ru>
2019-11-12 22:37:55 +0300

    [FIX] Minor login fix

--- AngularMVC/ClientApp/src/app/login/login.component.ts
+++ AngularMVC/ClientApp/src/app/login/login.component.ts
@@ -3,13 +3,14 @@ import {AuthenticationService} from "../services/auth.component";
 import construct = Reflect.construct;
 
 @Component({
-  templateUrl:"./login.component.html"
+  templateUrl: "./login.component.html"
 })
 
 export class LoginComponent {
-  constructor(private auth:AuthenticationService){}
-  login()
-  {
-    this.auth.login("lol","kek");
+  constructor(private auth: AuthenticationService) {
+  }
+
+  login(email: string, password: string) {
+    this.auth.login("lol", "kek");
   }
 }
